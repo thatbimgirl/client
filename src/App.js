f04977f6cad0fb2ab6652fc2a4d6ae0422580642
thatbimgirl <j.rohrmann@hotmail.de>
2023-03-09 18:36:24 +0100

    UI cok guezel

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ const Scene = () => {
   });
 
   console.log(obj);
-  return <primitive object={obj} scale={0.4} position={[-5, 0, 0]}/>;
+  return <primitive object={obj} scale={0.4} position={[-5, -1, 0]}/>;
 };
 
 /*
@@ -57,18 +57,7 @@ const Text = () =>(
 )
 */
 
-const height = "10%";
-const max = 50;
-const min = 0;
-const ecvalue = 25;
-const uncertainty = 10;
-const real_max = 500;
-const real_ecvalue = (ecvalue / max)*real_max;
-const real_uncertainty = (uncertainty / max)*real_max;
-const real_margin = real_ecvalue - (0.5* real_max);
-const aa = `${real_margin*2}px`
-const bb = `${real_uncertainty*2}px`
-//const bb= "240px";
+
 
 
 class App extends Component {
@@ -96,45 +85,72 @@ class App extends Component {
   
     handleSubmit(event) {
       //alert('A name was submitted: ' + this.state.value);
-      var a = this.state.apiResponse;
-      a= a.substring(1, a.length-1)
+      var footprint_from_api = this.state.apiResponse;
+      footprint_from_api= footprint_from_api.substring(1, footprint_from_api.length-1)
       if (this.state.value === 'concrete'){
-        var ec = a * 0.5;
-        document.getElementById("demo").innerHTML = "Embodied carbon: " + ec + " t CO2e";
+        var embodied_carbon = footprint_from_api * 0.5;
+        var uncertainty = 12;
+        document.getElementById("demo").innerHTML = "Embodied carbon: " + embodied_carbon + " t CO2e";
       } else if (this.state.value === 'steel'){
-        ec = a * 0.4;
-        document.getElementById("demo").innerHTML = "Embodied carbon: " + ec + " t CO2e";
+        embodied_carbon = footprint_from_api * 0.4;
+        uncertainty = 5;
+        document.getElementById("demo").innerHTML = "Embodied carbon: " + embodied_carbon + " t CO2e";
       } else if (this.state.value === 'wood'){
-        ec = a * -0.4;
-        document.getElementById("demo").innerHTML = "Embodied carbon: " + ec + " t CO2e";
+        embodied_carbon = footprint_from_api * 0.1;
+        uncertainty = 3;
+        document.getElementById("demo").innerHTML = "Embodied carbon: " + embodied_carbon + " t CO2e";
       }
       else if (this.state.value === 'masonry'){
-        ec = a * 0.3;
-        document.getElementById("demo").innerHTML = "Embodied carbon: " + ec + " t CO2e";
+        embodied_carbon = footprint_from_api * 0.3;
+        uncertainty = 2;
+        document.getElementById("demo").innerHTML = "Embodied carbon: " + embodied_carbon + " t CO2e";
       }
       else {
         document.getElementById("demo").innerHTML = "Embodied carbon: undetermined";
       }
       event.preventDefault();
 
-      document.getElementById("myRange").style.width=bb;
-      document.getElementById("myRange").style.marginLeft= aa;
+      
+      const standard_ec = footprint_from_api * 0.5;
+      const ecvalue = embodied_carbon;
+      const min = 0;
+      const max = Math.max(embodied_carbon, embodied_carbon+uncertainty, standard_ec)*1.2;
+      const real_max = 500;
+      const real_ecvalue = (ecvalue / max)*real_max;
+      const real_uncertainty = (uncertainty / max)*real_max;
+      const real_margin = real_ecvalue - (0.5* real_max);
+      const slider_offset = `${real_margin*2}px`;
+      const slider_width = `${real_uncertainty*2}px`;
+
+
+      document.getElementById("myRange").style.width=slider_width;
+      document.getElementById("myRange").style.marginLeft= slider_offset;
+      document.getElementById("left").innerHTML = min;
+      document.getElementById("middle").innerHTML = ecvalue;
+      document.getElementById("middle").style.marginLeft = `${real_ecvalue-10}px`;
+      document.getElementById("right").innerHTML = max.toFixed(2);
+      document.getElementById("market_standard_pointer").style.cx = `${standard_ec/max * real_max}`;
+      
+      
+      console.log(max, embodied_carbon, slider_offset, slider_width);
+      console.log(document.getElementById("myRange").style.width,  document.getElementById("myRange").style.marginLeft)
     }
 
-    render() {
 
+    render() {
+      const b = "hello";
         console.log(this.state.apiResponse);
         return (
            <div className="App">
               <div class = "lefthalf"> 
               <>    
-              <Canvas concurrent>
+              <Canvas concurrent camera={{ position: [-6, 0, 3], fov: 70 }}>
               <ambientLight intensity={0.1} />
               <directionalLight color="white" position={[0, 4, 5]} />
               <Suspense fallback={null}>
                   <Scene />
                   <OrbitControls />
-                  <Environment files="./img/kloppenheim.hdr" background blur={0.5} />
+                  <Environment files="./img/kloppenheim.hdr" background blur={0.7} />
               </Suspense>
               </Canvas>
               </>
@@ -156,7 +172,7 @@ class App extends Component {
                 </form>
                 <p id="demo">Embodied carbon:</p>
                 </div>
-                <p>Custom range slider:</p>
+
                 <div class = "container_row">
                   <div class="layer1">
                     <div class="slidecontainer" id ="slider1" >
@@ -166,8 +182,15 @@ class App extends Component {
                   <div class = "layer2">
                     <svg class = "rect" width="500px" height="20">
                     <rect  width="100%" height= "100%"/>
+                    <circle id="market_standard_pointer"  cx="247.5" cy="10" r="10" fill="hotpink" />
                     </svg>
+
                   </div>
+                  <text class = "scale_left" id="left" >0</text>
+                  <text class = "scale_middle" id = "middle">0</text>
+                  <text class = "scale_middle" id = "standard">0</text>
+                  <text class = "scale_right" id="right">0</text>
+
 
 
                 </div>
